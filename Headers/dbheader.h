c705670ea9771f59689ebf02744d2ab156ae5759
khawlaaaaaaa <132809565+khawlaaaaaaa@users.noreply.github.com>
2024-01-08 00:02:44 +0100

    Create dbheader.h

---
+++ Headers/dbheader.h
@@ -0,0 +1,11 @@
+        #ifndef DBHEADER_H
+
+#include <QSqlDatabase>
+#include <QSqlDriver>
+#include <QSqlError>
+#include <QDebug>
+#include <QSqlTableModel>
+#include <QSqlquery>
+#include <QFile>
+#include <QSqlQueryModel>
+#endif // DBHEADER_H
